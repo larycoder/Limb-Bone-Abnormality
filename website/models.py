aaf57b4c56e4667dba5e6bdf50ff024250390a39
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-12-08 22:40:12 +0700

    add data in models

--- website/models.py
+++ website/models.py
@@ -3,14 +3,23 @@ from flask_login import UserMixin
 from sqlalchemy.sql  import func
 
 class User(db.Model, UserMixin):
+    role = db.Column(db.Integer, default = 2)
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(50), nullable= False)
     username = db.Column(db.String(50))
     password = db.Column(db.String(50))
     files = db.relationship('File')
 
+
+class Folder(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    path = db.Column(db.String(50), nullable = False)
+    date = db.Column(db.DateTime(timezone = True), default=func.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 class File(db.Model):
     id = db.Column(db.Integer, primary_key= True)
-    data = db.Column(db.String(1000))
+    path = db.Column(db.String(50), nullable = False)
     date = db.Column(db.DateTime(timezone= True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    folder_id = db.Column(db.Integer, db.ForeignKey('folder.id'))
