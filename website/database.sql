17c679e33e2fa1a31e955c5563a1a48f68e3d802
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2023-12-10 16:00:53 +0700

    change file

--- website/database.sql
+++ website/database.sql
@@ -1,18 +1,18 @@
-drop database if exists Account;
-create database Account;
-use Account; 
-drop table if exists User;
-drop table if exists File;
-create table User(
-	id int(10) auto_increment primary key,
-    email text,
-    username text,
-    password text
-);
-create table File(
-	id int(10) auto_increment primary key,
-    data text,
-    date datetime,
-    user_id int(10),
-    foreign key(user_id) references User(id)
-)auto_increment = 1;
+-- drop database if exists Account;
+-- create database Account;
+-- use Account; 
+-- drop table if exists User;
+-- drop table if exists File;
+-- create table User(
+-- 	id int(10) auto_increment primary key,
+--     email text,
+--     username text,
+--     password text
+-- );
+-- create table File(
+-- 	id int(10) auto_increment primary key,
+--     data text,
+--     date datetime,
+--     user_id int(10),
+--     foreign key(user_id) references User(id)
+-- )auto_increment = 1;
