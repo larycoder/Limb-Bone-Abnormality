76ab753c7009cd62bc4acf29bb2773eebda7aa7f
NguyenXuanCuong-BI12-068 <cautraicuong@gmail.com>
2024-01-03 11:03:25 +0700

    add command

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import subprocess
 
-path = "bash.sh"
-arg1 = "arg1"
-arg2 = "arg2"
+# path = "bash.sh"
+# arg1 = "arg1"
+# arg2 = "arg2"
 
-try:
-    subprocess.run(["bash", path, arg1, arg2])
-    print("Bash script executed successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"Error executing Bash script: {e}")
+# try:
+#     subprocess.run(["bash", path, arg1, arg2])
+#     print("Bash script executed successfully.")
+# except subprocess.CalledProcessError as e:
+#     print(f"Error executing Bash script: {e}")
+
+# Define the command to execute the tool script with the "fatsq" file
+command = "./bash.sh HG001.hiseq4000.wes_truseq.50x.R1.fastq.gz"
+
+# Execute the command
+subprocess.run(command, shell=True)
