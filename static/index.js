d7da21b74a5cecb2f2dac1061922f7ba088ed7cc
Cường Xuân <cuongnx.bi12-068@st.usth.edu.vn>
2024-01-28 00:50:41 +0700

    reload page

--- static/index.js
+++ static/index.js
@@ -63,16 +63,22 @@ function executeF(Id){
             throw new Error("HTTP error! Status:",response.status);
         }
         return response.json();
-    }).then((res) => res.json)
-    .then(data=>{
-        if(data.updated){
-            location.reload
-        }
+    }).then(data=>{
         window.location.href = "/folder/"+ folder_id;
+    })
+
+    fetch('/folder/'+Id)  // Update the endpoint to match your route
+    .then(response => response.json())
+    .then(data => {
+      if (data.updated) {
+        // If an update is detected, reload the page
+        location.reload();
+      }
     })
     .catch(error => console.error('Error checking for updates:', error));
 }
-setInterval(executeF, 300000)
+  // Check for updates every 5 seconds (adjust the interval as needed)
+setInterval(executeF, 5000);
 
 function executeSubF(Id,folder_id){
     fetch('/executeSubF',{
